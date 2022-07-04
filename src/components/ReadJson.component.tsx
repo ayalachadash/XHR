c32ecd5e13ceeedb275f8daa785aba0fcd638cfd
ayalachadash <ayala0533121955@gmail.com>
2022-07-04 10:58:04 +0300

    now

--- src/components/ReadJson.component.tsx
+++ src/components/ReadJson.component.tsx
@@ -2,29 +2,28 @@ import React from 'react';
 
 
 function ReadJson() {
-    // let xmlhttp = new XMLHttpRequest();
-    // let url = "https://jsonplaceholder.typicode.com/posts";
-    // //http://localhost:3000/user/getall
+    let xmlhttp = new XMLHttpRequest();
+    let url = "https://jsonplaceholder.typicode.com/posts";
 
-    // const toReadJson = () => {
-    //     xmlhttp.onreadystatechange = function () {
-    //         if (this.readyState == 4 && this.status == 200) {
-    //             console.log(this.responseText);
-    //             console.log(this.status);
-    //             console.log(this.responseURL);
+    const toReadJson = () => {
+        xmlhttp.onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 200) {
+                console.log(this.responseText);
+                console.log(this.status);
+                console.log(this.responseURL);
 
-    //             const element: HTMLElement = document.getElementById('toShowJson') as HTMLElement
-    //             element.innerHTML = this.responseText;
-    //         }
-    //     };
-    //     xmlhttp.open("GET", url, true);
-    //     xmlhttp.send();
-    // }
+                const element: HTMLElement = document.getElementById('toShowJson') as HTMLElement
+                element.innerHTML = this.responseText;
+            }
+        };
+        xmlhttp.open("GET", url, true);
+        xmlhttp.send();
+    }
 
     return (
         <div>
             <p>hello read json</p>
-            {/* <button onClick={toReadJson}>json</button> */}
+            <button onClick={toReadJson}>json</button>
             <div id='toShowJson'></div>
         </div>
     )
